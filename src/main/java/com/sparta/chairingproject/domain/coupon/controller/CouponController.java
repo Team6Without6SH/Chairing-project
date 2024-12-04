0ab0294455183bd3bb0c5a1a8e9e7f92e6e40166
coldrice99 <coldrice99@gmail.com>
2024-12-04 16:38:57 +0900

    #3 feat: 어드민 권한 검증 추가

--- src/main/java/com/sparta/chairingproject/domain/coupon/controller/CouponController.java
+++ src/main/java/com/sparta/chairingproject/domain/coupon/controller/CouponController.java
@@ -1,12 +1,16 @@
 package com.sparta.chairingproject.domain.coupon.controller;
 
+import com.sparta.chairingproject.config.security.UserDetailsImpl;
 import com.sparta.chairingproject.domain.coupon.dto.CouponRequest;
 import com.sparta.chairingproject.domain.coupon.dto.CouponResponse;
 import com.sparta.chairingproject.domain.coupon.service.CouponService;
+import com.sparta.chairingproject.domain.member.entity.MemberRole;
 import com.sparta.chairingproject.util.ResponseBodyDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +23,8 @@ public class CouponController {
     private final CouponService couponService;
 
     @PostMapping
-    public ResponseEntity<ResponseBodyDto<CouponResponse>> createCoupon(@Valid @RequestBody CouponRequest request) {
-        CouponResponse response = couponService.createCoupon(request);
+    public ResponseEntity<ResponseBodyDto<CouponResponse>> createCoupon(@Valid @RequestBody CouponRequest request, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CouponResponse response = couponService.createCoupon(request, userDetails.getMember());
         return ResponseEntity.ok(ResponseBodyDto.success("쿠폰 생성 성공", response));
     }
 }
--- src/main/java/com/sparta/chairingproject/domain/coupon/service/CouponService.java
+++ src/main/java/com/sparta/chairingproject/domain/coupon/service/CouponService.java
@@ -1,20 +1,24 @@
 package com.sparta.chairingproject.domain.coupon.service;
 
-import com.sparta.chairingproject.domain.coupon.controller.CouponController;
 import com.sparta.chairingproject.domain.coupon.dto.CouponRequest;
 import com.sparta.chairingproject.domain.coupon.dto.CouponResponse;
 import com.sparta.chairingproject.domain.coupon.entity.Coupon;
 import com.sparta.chairingproject.domain.coupon.repository.CouponRepository;
+import com.sparta.chairingproject.domain.member.entity.Member;
+import com.sparta.chairingproject.domain.member.entity.MemberRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CouponService {
     private final CouponRepository couponRepository;
 
-    public CouponResponse createCoupon(CouponRequest request) {
+    public CouponResponse createCoupon(CouponRequest request, Member member) {
+        if(!MemberRole.ADMIN.equals(member.getMemberRole())) {
+            throw new IllegalStateException("쿠폰 생성 권한이 없습니다."); // TODO: 글로벌 예외처리
+        }
+
         Coupon coupon = Coupon.builder()
                 .name(request.name())
                 .quantity(request.quantity())
