518b9dd423ac8f03443a73f16e33b75030171550
syh213 <syh21133@gmail.com>
2024-12-05 13:02:22 +0900

    feat : 내 주문내역 조회 기능 추가
    1. 주문 내역 조회 요청
    2. 토큰에 있는 ID로 주문 테이블 조회
    3. 조회된 정보 반환(가게이름, 가격, 현재 상태)

--- src/main/java/com/sparta/chairingproject/domain/order/repository/OrderRepository.java
+++ src/main/java/com/sparta/chairingproject/domain/order/repository/OrderRepository.java
@@ -1,10 +1,18 @@
 package com.sparta.chairingproject.domain.order.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.sparta.chairingproject.domain.order.entity.Order;
 import com.sparta.chairingproject.domain.order.entity.OrderStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	int countByStoreIdAndStatus(Long storeId, OrderStatus orderStatus);
+
+
+    @Query("SELECT o FROM Order o JOIN o.member m JOIN o.menus menu JOIN menu.store s WHERE m.id = :memberId")
+    Page<Order> findByMember(@Param("memberId") Long memberId, Pageable pageable);
+
 }
