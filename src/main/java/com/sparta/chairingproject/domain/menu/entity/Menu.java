757a06513f5817c19dd76ef304c35b309aabb787
syh213 <syh21133@gmail.com>
2024-12-06 12:12:11 +0900

    feat : 멤버 서비스 테스트 코드
    # Conflicts:
    #	src/main/java/com/sparta/chairingproject/domain/coupon/entity/Coupon.java
    #	src/main/java/com/sparta/chairingproject/domain/member/entity/Member.java

--- src/main/java/com/sparta/chairingproject/domain/menu/entity/Menu.java
+++ src/main/java/com/sparta/chairingproject/domain/menu/entity/Menu.java
@@ -2,7 +2,6 @@ package com.sparta.chairingproject.domain.menu.entity;
 
 import com.sparta.chairingproject.domain.common.entity.Timestamped;
 import com.sparta.chairingproject.domain.store.entity.Store;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,28 +10,31 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "menus")
 public class Menu extends Timestamped {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 
-	@Column(nullable = false)
-	private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(nullable = false)
-	private int price;
+    @Column(nullable = false)
+    private String name;
 
-	@Column(nullable = true)
-	private String image;
+    @Column(nullable = false)
+    private int price;
 
-	@ManyToOne
-	@JoinColumn(name = "store_id", nullable = false)
-	private Store store;
+    @Column(nullable = true)
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
 }
