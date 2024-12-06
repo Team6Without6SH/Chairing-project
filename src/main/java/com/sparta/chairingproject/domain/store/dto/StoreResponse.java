a5f63fd0687b227be0288c6aed51391ddfd8370d
jangms3 <github.com/jangms3>
2024-12-06 20:21:32 +0900

    fix: 요청 사항 수리
    - 수리 완료

--- src/main/java/com/sparta/chairingproject/domain/store/dto/StoreResponse.java
+++ src/main/java/com/sparta/chairingproject/domain/store/dto/StoreResponse.java
@@ -10,20 +10,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StoreResponse {
-	private Long id;                  // 가게 ID
-	private String name;              // 가게 이름
-	private String address;           // 가게 주소
-	private String phone;             // 연락처
-	private String openTime;          // 영업 시작 시간
-	private String closeTime;         // 영업 종료 시간
-	private List<String> categories;  // 카테고리 리스트
-	private String description;       // 가게 설명
-	private List<String> images;      // 이미지 리스트
-	private String ownerName;         // 사장님 이름
+	private Long id;
+	private String name;
+	private String address;
+	private String phone;
+	private String openTime;
+	private String closeTime;
+	private List<String> categories;
+	private String description;
+	private List<String> images;
+	private String ownerName;
 
 	public StoreResponse(String name, String image, String description) {
 		this.name = name;
-		this.images = List.of(image); // 단일 이미지를 리스트로 변환
+		this.images = List.of(image);
 		this.description = description;
 	}
 }
