c9489f5fba7059017ed26047bb9852353ae2469f
ethrad <asdf123f4f4@gmail.com>
2024-12-09 21:32:22 +0900

    style: 네이버 코드 컨벤션 적용

--- src/main/java/com/sparta/chairingproject/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/sparta/chairingproject/domain/reservation/controller/ReservationController.java
@@ -6,8 +6,10 @@ import com.sparta.chairingproject.domain.reservation.dto.request.CreateReservati
 import com.sparta.chairingproject.domain.reservation.dto.request.UpdateReservationRequest;
 import com.sparta.chairingproject.domain.reservation.dto.response.ReservationResponse;
 import com.sparta.chairingproject.domain.reservation.service.ReservationService;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -18,41 +20,44 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ReservationController {
 
-    private final ReservationService reservationService;
+	private final ReservationService reservationService;
 
-    /* 일반 사용자 */
+	/* 일반 사용자 */
 
-    @Secured("ROLE_USER")
-    @PostMapping("/stores/{storeId}/reservations")
-    public ResponseEntity<ReservationResponse> createReservation(@PathVariable Long storeId,
-        @Valid @RequestBody CreateReservationRequest req,
-        @AuthenticationPrincipal UserDetailsImpl authUser) {
-        return new ResponseEntity<>(
-            reservationService.createReservation(storeId, req, authUser),
-            HttpStatus.OK);
-    }
+	@Secured("ROLE_USER")
+	@PostMapping("/stores/{storeId}/reservations")
+	public ResponseEntity<ReservationResponse> createReservation(
+		@PathVariable Long storeId,
+		@Valid @RequestBody CreateReservationRequest req,
+		@AuthenticationPrincipal UserDetailsImpl authUser) {
+		return new ResponseEntity<>(
+			reservationService.createReservation(storeId, req, authUser),
+			HttpStatus.OK);
+	}
 
-    @Secured("ROLE_USER")
-    @DeleteMapping("/members/reservations/{reservationId}")
-    public ResponseEntity<ReservationResponse> cancelReservation(@PathVariable Long reservationId,
-                                                                 @RequestBody RequestDto req,
-                                                                 @AuthenticationPrincipal UserDetailsImpl authUser) {
-        return new ResponseEntity<>(
-                reservationService.cancelReservation(reservationId, req, authUser),
-                HttpStatus.OK);
-    }
+	@Secured("ROLE_USER")
+	@DeleteMapping("/members/reservations/{reservationId}")
+	public ResponseEntity<ReservationResponse> cancelReservation(
+		@PathVariable Long reservationId,
+		@RequestBody RequestDto req,
+		@AuthenticationPrincipal UserDetailsImpl authUser) {
+		return new ResponseEntity<>(
+			reservationService.cancelReservation(reservationId, req, authUser),
+			HttpStatus.OK);
+	}
 
 
-    /* 사장 */
+	/* 사장 */
 
-    @Secured("ROLE_OWNER")
-    @PatchMapping("/stores/{storeId}/reservations/{reservationId}")
-    public ResponseEntity<ReservationResponse> updateReservation(@PathVariable Long storeId,
-                                                                 @PathVariable Long reservationId,
-                                                                 @Valid @RequestBody UpdateReservationRequest req,
-                                                                 @AuthenticationPrincipal UserDetailsImpl authUser) {
-        return new ResponseEntity<>(
-                reservationService.updateReservation(storeId, reservationId, req, authUser),
-                HttpStatus.OK);
-    }
+	@Secured("ROLE_OWNER")
+	@PatchMapping("/stores/{storeId}/reservations/{reservationId}")
+	public ResponseEntity<ReservationResponse> updateReservation(
+		@PathVariable Long storeId,
+		@PathVariable Long reservationId,
+		@Valid @RequestBody UpdateReservationRequest req,
+		@AuthenticationPrincipal UserDetailsImpl authUser) {
+		return new ResponseEntity<>(
+			reservationService.updateReservation(storeId, reservationId, req, authUser),
+			HttpStatus.OK);
+	}
 }
--- src/main/java/com/sparta/chairingproject/domain/reservation/dto/response/ReservationResponse.java
+++ src/main/java/com/sparta/chairingproject/domain/reservation/dto/response/ReservationResponse.java
@@ -1,6 +1,7 @@
 package com.sparta.chairingproject.domain.reservation.dto.response;
 
 import com.sparta.chairingproject.domain.reservation.entity.ReservationStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,20 +11,20 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 public class ReservationResponse {
-    private Long id;
+	private Long id;
 
-    private Long memberId;
+	private Long memberId;
 
-    private Long storeId;
+	private Long storeId;
 
-    private int guestCount;
+	private int guestCount;
 
-    private LocalDate date;
+	private LocalDate date;
 
-    private String time;
+	private String time;
 
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
+	private LocalDateTime createdAt;
+	private LocalDateTime modifiedAt;
 
-    private ReservationStatus status;
+	private ReservationStatus status;
 }
