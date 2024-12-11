d449db6bfba7fb5ed3f44937f76746db1226a018
ethrad <asdf123f4f4@gmail.com>
2024-12-11 16:39:37 +0900

    Fix: test member의 생성자 사용 부분 수정

--- src/test/java/com/sparta/chairingproject/util/AuthUtilsTest.java
+++ src/test/java/com/sparta/chairingproject/util/AuthUtilsTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.sparta.chairingproject.config.exception.customException.GlobalException;
 import com.sparta.chairingproject.config.exception.enums.ExceptionCode;
@@ -35,7 +36,8 @@ class AuthUtilsTest {
 
 	@BeforeEach
 	void setUp() {
-		testMember = new Member(1L, "Test User", "test@example.com", "encodedPassword", MemberRole.USER);
+		testMember = new Member("Test User", "test@example.com", "encodedPassword", MemberRole.USER);
+		ReflectionTestUtils.setField(testMember, "id", 1L);
 		authUser = new UserDetailsImpl(testMember);
 	}
 
