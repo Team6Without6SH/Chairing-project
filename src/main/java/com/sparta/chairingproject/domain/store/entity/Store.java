0381be687836d7c7cbcc20a7a983ab3fb89aa493
syh213 <syh21133@gmail.com>
2024-12-11 15:22:08 +0900

    refactor : 코드 수정
    불필요한 코드 수정 및 삭제

--- src/main/java/com/sparta/chairingproject/domain/store/entity/Store.java
+++ src/main/java/com/sparta/chairingproject/domain/store/entity/Store.java
@@ -34,100 +34,100 @@ import lombok.Setter;
 @Table(name = "stores")
 public class Store extends Timestamped {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    private String name;
+	@Column(nullable = false)
+	private String name;
 
-    @Column(nullable = true)
-    private String image;
+	@Column(nullable = true)
+	private String image;
 
-    @Column(nullable = true)
-    private String description;
+	@Column(nullable = true)
+	private String description;
 
-    @Column(nullable = true)
-    private String address;
+	@Column(nullable = true)
+	private String address;
 
-    @Column(nullable = true)
-    private String phone;
+	@Column(nullable = true)
+	private String phone;
 
-    @Column(nullable = true)
-    private String openTime;
+	@Column(nullable = true)
+	private String openTime;
 
-    @Column(nullable = true)
-    private String closeTime;
+	@Column(nullable = true)
+	private String closeTime;
 
-    private String category;
+	private String category;
 
-    @Column(nullable = false)
-    private int tableCount;
+	@Column(nullable = false)
+	private int tableCount;
 
-    @Column
-    private LocalDateTime deletedAt = null;
+	@Column
+	private LocalDateTime deletedAt = null;
 
-    @ManyToOne
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member owner;
-    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Menu> menus = new ArrayList<>();
-    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Review> reviews = new ArrayList<>();
-    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Reservation> reservations = new ArrayList<>();
-    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Order> orders = new ArrayList<>();
-    @Setter
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private StoreStatus status = StoreStatus.PENDING;
-    @Setter
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private StoreRequestStatus requestStatus = StoreRequestStatus.PENDING;
+	@ManyToOne
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member owner;
+	@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Menu> menus = new ArrayList<>();
+	@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Review> reviews = new ArrayList<>();
+	@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Reservation> reservations = new ArrayList<>();
+	@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Order> orders = new ArrayList<>();
+	@Setter
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private StoreStatus status = StoreStatus.PENDING;
+	@Setter
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private StoreRequestStatus requestStatus = StoreRequestStatus.PENDING;
 
-    // 매개변수 생성자 (서비스)
-    public Store(String name, String image, String description, String address, Member owner) {
-        this.name = name;
-        this.image = image;
-        this.description = description;
-        this.address = address;
-        this.owner = owner;
-    }
+	// 매개변수 생성자 (서비스)
+	public Store(String name, String image, String description, String address, Member owner) {
+		this.name = name;
+		this.image = image;
+		this.description = description;
+		this.address = address;
+		this.owner = owner;
+	}
 
-    // 상태 업데이트 메서드
-    public void approveRequest() {
-        this.requestStatus = StoreRequestStatus.APPROVED;
-        this.status = StoreStatus.OPEN;
-    }
+	// 상태 업데이트 메서드
+	public void approveRequest() {
+		this.requestStatus = StoreRequestStatus.APPROVED;
+		this.status = StoreStatus.OPEN;
+	}
 
-    public void rejectRequest() {
-        this.requestStatus = StoreRequestStatus.REJECTED;
-    }
+	public void rejectRequest() {
+		this.requestStatus = StoreRequestStatus.REJECTED;
+	}
 
-    public void updateStore(UpdateStoreRequest req) {
-        this.name = req.getName();
-        this.address = req.getAddress();
-        this.phone = req.getPhone();
-        this.openTime = req.getOpenTime();
-        this.closeTime = req.getCloseTime();
-        this.category = req.getCategory();
-        this.description = req.getDescription();
-        this.image = req.getImage();
-    }
+	public void updateStore(UpdateStoreRequest req) {
+		this.name = req.getName();
+		this.address = req.getAddress();
+		this.phone = req.getPhone();
+		this.openTime = req.getOpenTime();
+		this.closeTime = req.getCloseTime();
+		this.category = req.getCategory();
+		this.description = req.getDescription();
+		this.image = req.getImage();
+	}
 
-    public void approveDelete() {
-        this.status = StoreStatus.CLOSED;
-        this.requestStatus = StoreRequestStatus.APPROVED;
-        this.deletedAt = LocalDateTime.now();
-    }
+	public void approveDelete() {
+		this.status = StoreStatus.CLOSED;
+		this.requestStatus = StoreRequestStatus.APPROVED;
+		this.deletedAt = LocalDateTime.now();
+	}
 
-    public boolean isDeleted() {
-        return this.requestStatus == StoreRequestStatus.DELETED;
-    }
+	public boolean isDeleted() {
+		return this.requestStatus == StoreRequestStatus.DELETED;
+	}
 
-    public void rejectDeleteRequest() {
-        this.requestStatus = StoreRequestStatus.DELETE_REJECTED;
-    }
+	public void rejectDeleteRequest() {
+		this.requestStatus = StoreRequestStatus.DELETE_REJECTED;
+	}
 }
