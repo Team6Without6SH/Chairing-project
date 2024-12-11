cb3069d56a2b11c845c15e0fda3ebe2a21b62d5e
Minwoo <alsdn2606@gmail.com>
2024-12-11 20:29:19 +0900

    test: 주문 생성 로직 통합테스트 작성
    - 정상 주문 생성: 여유 테이블 있을 때
    - 정상 주문 생성: 메뉴 없이 요청
    - 존재하지 않는 가게로 주문 요청
    - 잘못된 메뉴 포함된 주문 요청
    - 결제 금액 불일치
    - 여유 테이블이 없는 경우는 WAITING 상태로 생성

--- src/main/java/com/sparta/chairingproject/domain/order/dto/response/OrderResponse.java
+++ src/main/java/com/sparta/chairingproject/domain/order/dto/response/OrderResponse.java
@@ -2,11 +2,14 @@ package com.sparta.chairingproject.domain.order.dto.response;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderResponse {
 	private Long orderId;
 	private String orderStatus;
