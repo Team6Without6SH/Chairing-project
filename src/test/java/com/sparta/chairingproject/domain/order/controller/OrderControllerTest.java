eb0fd905a364cb02b35bdb71ee96beb94bed40ae
Minwoo <alsdn2606@gmail.com>
2024-12-12 11:53:35 +0900

    fix: 주문 생성 로직 통합테스트 H2 적용 전체 구동확인
    - 제목과 같음

--- src/test/java/com/sparta/chairingproject/domain/order/controller/OrderControllerTest.java
+++ src/test/java/com/sparta/chairingproject/domain/order/controller/OrderControllerTest.java
@@ -4,23 +4,24 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.net.URLEncoder;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sparta.chairingproject.config.JwtUtil;
 import com.sparta.chairingproject.config.security.UserDetailsImpl;
 import com.sparta.chairingproject.domain.member.entity.Member;
 import com.sparta.chairingproject.domain.member.entity.MemberRole;
@@ -34,18 +35,16 @@ import com.sparta.chairingproject.domain.order.repository.OrderRepository;
 import com.sparta.chairingproject.domain.store.entity.Store;
 import com.sparta.chairingproject.domain.store.repository.StoreRepository;
 
-import jakarta.servlet.http.Cookie;
-
 @TestPropertySource(locations = "/application-test.properties")
 @SpringBootTest
-@AutoConfigureMockMvc
 @Transactional
 class OrderControllerTest {
 
-	@Autowired
 	private MockMvc mockMvc;
 
 	@Autowired
+	private WebApplicationContext context;
+
 	private ObjectMapper objectMapper;
 
 	@Autowired
@@ -60,41 +59,42 @@ class OrderControllerTest {
 	@Autowired
 	private OrderRepository orderRepository;
 
-	@Autowired
-	private JwtUtil jwtUtil;
-
-	private UserDetailsImpl authMember;
-	private Member member;
-	private Member owner;
+	private Member testMember;
+	private Member testOwner;
 	private Store store;
 	private Menu menu1;
 	private Menu menu2;
-	private String jwtToken;
 
 	@BeforeEach
 	void setUp() {
-		member = memberRepository.save(new Member("T member", "test@email.com", "password1234", MemberRole.USER));
-		owner = memberRepository.save(new Member("T Owner", "testOwner@email.com", "password1234", MemberRole.OWNER));
-		store = storeRepository.save(new Store("T Store", "testImage.jpg", "Test store death", "seoul", owner));
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		objectMapper = new ObjectMapper();
+
+		testMember = new Member("T member", "test@email.com", "password1234", MemberRole.USER);
+		testOwner = new Member("T Owner", "testOwner@email.com", "password1234", MemberRole.OWNER);
+		store = new Store("T Store", "testImage.jpg", "Test store death", "seoul", testOwner);
 		ReflectionTestUtils.setField(store, "tableCount", 5);
-		menu1 = menuRepository.save(Menu.createOf("menu1", 10000, "menuImage.jpg", store));
-		ReflectionTestUtils.setField(menu1, "id", 1L);
-		menu2 = menuRepository.save(Menu.createOf("menu2", 20000, "menuImage2.jpg", store));
-		ReflectionTestUtils.setField(menu2, "id", 2L);
-		jwtToken = jwtUtil.createToken(member.getId(), member.getEmail(), member.getMemberRole());
+		menu1 = Menu.createOf("menu1", 10000, "menuImage.jpg", store);
+		menu2 = Menu.createOf("menu2", 20000, "menuImage2.jpg", store);
+
+		memberRepository.save(testMember);
+		memberRepository.save(testOwner);
+		storeRepository.save(store);
+		menuRepository.save(menu1);
+		menuRepository.save(menu2);
 	}
 
 	@Test
 	@DisplayName("정상 주문 생성 - 여유 테이블 있을 때")
 	void createOrder_success() throws Exception {
-		jwtToken = URLEncoder.encode(jwtToken, "UTF-8");
 
-		OrderRequest orderRequest = new OrderRequest(List.of(1L, 2L), 30000);
+		OrderRequest orderRequest = new OrderRequest(List.of(menu1.getId(), menu2.getId()), 30000);
+		setAuthentication(testMember);
 
 		mockMvc.perform(post("/stores/" + store.getId() + "/orders")
+				.principal(() -> testMember.getEmail())
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(orderRequest))
-				.cookie(new Cookie("Authorization", jwtToken)))
+				.content(objectMapper.writeValueAsString(orderRequest)))
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isCreated())
 			.andExpect(jsonPath("$.orderStatus").value("ADMISSION"))
@@ -107,14 +107,14 @@ class OrderControllerTest {
 	@Test
 	@DisplayName("정상 주문 생성 - 메뉴 없이 요청")
 	void createOrder_NoMenus() throws Exception {
-		jwtToken = URLEncoder.encode(jwtToken, "UTF-8");
 
 		OrderRequest orderRequest = new OrderRequest(List.of(), 0);
+		setAuthentication(testMember);
 
 		mockMvc.perform(post("/stores/" + store.getId() + "/orders")
+				.principal(() -> testMember.getEmail())
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(orderRequest))
-				.cookie(new Cookie("Authorization", jwtToken)))
+				.content(objectMapper.writeValueAsString(orderRequest)))
 			.andExpect(status().isCreated())
 			.andExpect(jsonPath("$.orderStatus").value("ADMISSION")) //현재 따로 IN_PROGRESS 인 사람이 없기 때문
 			.andExpect(jsonPath("$.totalPrice").value(0))
@@ -125,11 +125,12 @@ class OrderControllerTest {
 	@DisplayName("존재하지 않는 가게로 주문 요청")
 	void createOrder_NotFoundStore() throws Exception {
 		OrderRequest orderRequest = new OrderRequest(List.of(menu1.getId()), 10000);
+		setAuthentication(testMember);
 
 		mockMvc.perform(post("/stores/9999/orders")
+				.principal(() -> testMember.getEmail())
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(orderRequest))
-				.cookie(new Cookie("Authorization", jwtToken)))
+				.content(objectMapper.writeValueAsString(orderRequest)))
 			.andExpect(status().isNotFound())
 			.andExpect(jsonPath("$.code").value("NOT_FOUND_STORE"));
 	}
@@ -138,11 +139,12 @@ class OrderControllerTest {
 	@DisplayName("잘못된 메뉴 포함된 주문 요청")
 	void createOrder_NotFoundMenu() throws Exception {
 		OrderRequest orderRequest = new OrderRequest(List.of(menu1.getId(), 999L), 3000);
+		setAuthentication(testMember);
 
 		mockMvc.perform(post("/stores/" + store.getId() + "/orders")
+				.principal(() -> testMember.getEmail())
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(orderRequest))
-				.cookie(new Cookie("Authorization", jwtToken)))
+				.content(objectMapper.writeValueAsString(orderRequest)))
 			.andExpect(status().isBadRequest())
 			.andExpect(jsonPath("$.code").value("NOT_ORDER_THIS_STORE"));
 	}
@@ -151,11 +153,12 @@ class OrderControllerTest {
 	@DisplayName("결제 금액 불일치")
 	void createOrder_PaymentNotMatched() throws Exception {
 		OrderRequest orderRequest = new OrderRequest(List.of(menu1.getId(), menu2.getId()), 4000);
+		setAuthentication(testMember);
 
 		mockMvc.perform(post("/stores/" + store.getId() + "/orders")
+				.principal(() -> testMember.getEmail())
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(orderRequest))
-				.cookie(new Cookie("Authorization", jwtToken)))
+				.content(objectMapper.writeValueAsString(orderRequest)))
 			.andExpect(status().isBadRequest())
 			.andExpect(jsonPath("$.code").value("PAYED_NOT_EQUAL_BILL"));
 	}
@@ -164,17 +167,25 @@ class OrderControllerTest {
 	@DisplayName("여유 테이블이 없는 경우는 WAITING 상태로 생성")
 	void createOrder_Waiting() throws Exception {
 		for (int i = 0; i < 5; i++) {
-			orderRepository.save(Order.createOf(member, store, List.of(), OrderStatus.IN_PROGRESS, 0));
+			orderRepository.save(Order.createOf(testMember, store, List.of(), OrderStatus.IN_PROGRESS, 0));
 		}
 		OrderRequest orderRequest = new OrderRequest(List.of(menu1.getId(), menu2.getId()), 30000);
+		setAuthentication(testMember);
 
 		mockMvc.perform(post("/stores/" + store.getId() + "/orders")
+				.principal(() -> testMember.getEmail())
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(orderRequest))
-				.cookie(new Cookie("Authorization", jwtToken)))
+				.content(objectMapper.writeValueAsString(orderRequest)))
 			.andExpect(status().isCreated())
 			.andExpect(jsonPath("$.orderStatus").value("WAITING"))
 			.andExpect(jsonPath("$.totalPrice").value(30000));
 
+	}
+
+	private void setAuthentication(Member member) {
+		UserDetailsImpl authMember = new UserDetailsImpl(testMember);
+		SecurityContextHolder.getContext().setAuthentication(
+			new UsernamePasswordAuthenticationToken(authMember, null, authMember.getAuthorities())
+		);
 	}
 }
