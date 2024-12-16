a490ae5a05cd5530f980409314b68b528b3968be
jangms3 <toldqn@naver.com>
2024-12-16 15:38:19 +0900

    feat: EnableCaching 추가 + 디버깅 추가
    - 캐싱 처리 성공적
    - MenuSummaryResponse & ReviewResponse 에 직렬화 implements 추가

--- src/main/java/com/sparta/chairingproject/domain/review/dto/ReviewResponse.java
+++ src/main/java/com/sparta/chairingproject/domain/review/dto/ReviewResponse.java
@@ -1,5 +1,7 @@
 package com.sparta.chairingproject.domain.review.dto;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ReviewResponse {
+public class ReviewResponse implements Serializable {
 	private String userName;  // 작성자 이름
 	private String content;   // 리뷰 내용
 	private int rating;       // 평점
