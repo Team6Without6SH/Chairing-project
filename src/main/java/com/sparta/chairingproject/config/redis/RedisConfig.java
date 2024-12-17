4515eeb8c4c94a81abad80176784cf78742f99e3
Minwoo <alsdn2606@gmail.com>
2024-12-17 18:29:48 +0900

    refactor: 안쓰는 클래스 및 파라미터 살짝 수정
    - 안쓰는 클래스 및 파라미터 살짝 수정

--- src/main/java/com/sparta/chairingproject/config/redis/RedisConfig.java
+++ src/main/java/com/sparta/chairingproject/config/redis/RedisConfig.java
@@ -6,16 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import com.sparta.chairingproject.domain.order.subscriber.OrderStatusSubscriber;
 
 @Configuration
 @EnableRedisRepositories
@@ -51,7 +45,7 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisMessageListenerContainer redisMessageListenerContainer(LettuceConnectionFactory connectionFactory) {
+	public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
 		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
 		container.setConnectionFactory(connectionFactory);
 		return container;
