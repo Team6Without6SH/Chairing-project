a64cdd57c8e53320c95da22e8e1bc2a946081ac0
Minwoo <alsdn2606@gmail.com>
2024-12-17 15:03:15 +0900

    feat: Redis pub/sub 구조 구현 및 WebSocket 연결
    - 사용자가 주문을 하는 시점에 Redis 의 채널을 구독하게 됨
    - 사장님이 주문 상태를 변경하면 메세지를 유저의 채널에 발행함
    - 유저는 WebSocket 으로 메세지를 확인할 수 있음

--- src/main/java/com/sparta/chairingproject/domain/order/subscriber/OrderStatusSubscriber.java
+++ src/main/java/com/sparta/chairingproject/domain/order/subscriber/OrderStatusSubscriber.java
@@ -2,26 +2,37 @@ package com.sparta.chairingproject.domain.order.subscriber;
 
 import java.io.IOException;
 
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
 import com.sparta.chairingproject.config.websocket.WebSocketHandler;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@Component
-public class OrderStatusSubscriber {
-	private final WebSocketHandler webSocketHandler;
-
-	public OrderStatusSubscriber(WebSocketHandler webSocketHandler) {
-		this.webSocketHandler = webSocketHandler;
-	}
-
-	public void handleMessage(String message) {
-		try {
-			webSocketHandler.broadcastMessage(message);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-}
+// @Slf4j
+// @Component
+// @RequiredArgsConstructor
+// public class OrderStatusSubscriber implements MessageListener {
+// 	private final WebSocketHandler webSocketHandler;
+//
+// 	@Override
+// 	public void onMessage(Message message, byte[] pattern) {
+// 		String channel = new String(pattern);
+// 		String payload = new String(message.getBody());
+// 		log.info("Redis 메시지 수신: channel={}, payload={}", channel, payload);
+//
+// 		if (channel.startsWith("order-status:member:")) {
+// 			String memberIdStr = channel.split(":")[2];
+// 			Long memberId = Long.valueOf(memberIdStr);
+// 			try {
+// 				webSocketHandler.sendMessageToUser(memberId, payload);
+// 			} catch (IOException e) {
+// 				log.error("WebSocket 메시지 전송 중 오류 발생", e);
+// 			}
+// 		} else {
+// 			log.warn("알 수 없는 채널: {}", channel);
+// 		}
+// 	}
+// }
