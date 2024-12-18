1358078824b139406d2f2362624a8711d2d64c47
jangms3 <toldqn@naver.com>
2024-12-18 14:58:44 +0900

    feat: eager loading 추가

--- src/main/java/com/sparta/chairingproject/domain/store/entity/Store.java
+++ src/main/java/com/sparta/chairingproject/domain/store/entity/Store.java
@@ -17,6 +17,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -73,10 +74,10 @@ public class Store extends Timestamped {
 	@JoinColumn(name = "member_id", nullable = false)
 	private Member owner;
 
-	@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
 	private List<Menu> menus = new ArrayList<>();
 
-	@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
 	private List<Review> reviews = new ArrayList<>();
 
 	@OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
