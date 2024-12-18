19151718b326aee192f0ea2883002748de499e67
Minwoo <alsdn2606@gmail.com>
2024-12-18 12:47:56 +0900

    feat: Caffeine Cache 적용
    - Caffeine Cache 를 이용하여 메세지마다의 TTL을 걸어서 휘발될 수 있게 설정
    - 메세지가 계속 메모리에 저장되지 않게 해서 메모리 누수 방지

--- src/main/java/com/sparta/chairingproject/domain/order/service/OrderNotificationService.java
+++ src/main/java/com/sparta/chairingproject/domain/order/service/OrderNotificationService.java
@@ -1,10 +1,11 @@
 package com.sparta.chairingproject.domain.order.service;
 
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.sparta.chairingproject.config.websocket.WebSocketHandler;
 
 import lombok.RequiredArgsConstructor;
@@ -13,19 +14,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrderNotificationService {
 	private final WebSocketHandler webSocketHandler;
-	private final Set<String> processedMessages = ConcurrentHashMap.newKeySet();
+	// Caffeine Cache 를 사용해서 Message 마다 10 분의 TTL 을 걸기
+	private final Cache<String, Boolean> processedMessages = Caffeine.newBuilder()
+		.expireAfterWrite(10, TimeUnit.MINUTES)
+		.build();
 
 	public void sendNotification(Long memberId, String message) {
 		String messageId = extractMessageId(message);
-		if (processedMessages.contains(messageId)) {
+		if (processedMessages.getIfPresent(messageId) != null) {
 			System.out.println("중복 메세지 필터링: Message Id: " + messageId);
 			return;
 		}
 		// WebSocket 에 메세지 전송
 		webSocketHandler.sendMessageToUser(memberId, message);
 
-		// 메세지 ID 를 Set 에 저장
-		processedMessages.add(messageId);
+		// 메세지 ID 를 Caffeine Cache 로 각 메세지마다의 TTL 을 걸수있음
+		processedMessages.put(messageId, true);
 		System.out.println("WebSocket 송신: memberId= " + memberId + ", message= " + message);
 	}
 
