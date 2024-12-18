a8eb693ed101a8741dd19a24e1153d31815ecbae
Minwoo <alsdn2606@gmail.com>
2024-12-18 12:08:33 +0900

    feat: 중복 메세지 필터링 기능 추가
    - Redis 메세지 발행 기능과 WebSocket 메세지 전송을 분리한 후
    - WebSocket 으로 메세지를 보내는 과정에서 Set 구조를 활용하여 메세지 중복전송 검증

--- src/main/java/com/sparta/chairingproject/domain/order/service/OrderNotificationService.java
+++ src/main/java/com/sparta/chairingproject/domain/order/service/OrderNotificationService.java
@@ -1,5 +1,8 @@
 package com.sparta.chairingproject.domain.order.service;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.stereotype.Service;
 
 import com.sparta.chairingproject.config.websocket.WebSocketHandler;
@@ -10,9 +13,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrderNotificationService {
 	private final WebSocketHandler webSocketHandler;
+	private final Set<String> processedMessages = ConcurrentHashMap.newKeySet();
 
 	public void sendNotification(Long memberId, String message) {
+		String messageId = extractMessageId(message);
+		if (processedMessages.contains(messageId)) {
+			System.out.println("중복 메세지 필터링: Message Id: " + messageId);
+			return;
+		}
+		// WebSocket 에 메세지 전송
 		webSocketHandler.sendMessageToUser(memberId, message);
+
+		// 메세지 ID 를 Set 에 저장
+		processedMessages.add(messageId);
 		System.out.println("WebSocket 송신: memberId= " + memberId + ", message= " + message);
+	}
+
+	private String extractMessageId(String message) {
+		return message.hashCode() + "";
 	}
 }
