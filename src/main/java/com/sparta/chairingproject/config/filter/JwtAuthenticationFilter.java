f490347725da3ef69caf0332d3945c7f533d2975
syh213 <syh21133@gmail.com>
2024-12-30 13:14:53 +0900

    refactor : 이미지 업로드

--- src/main/java/com/sparta/chairingproject/config/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/chairingproject/config/filter/JwtAuthenticationFilter.java
@@ -29,65 +29,67 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "로그인 및 JWT 생성")
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private final JwtUtil jwtUtil;
-    private final FcmServiceImpl fcmService;
+	private final JwtUtil jwtUtil;
+	private final FcmServiceImpl fcmService;
 
-    private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
 
 
-    public JwtAuthenticationFilter(JwtUtil jwtUtil, MemberRepository memberRepository, FcmServiceImpl fcmService) {
-        this.jwtUtil = jwtUtil;
-        this.memberRepository = memberRepository;
-        this.fcmService = fcmService;
-        setFilterProcessesUrl("/auth/signin");
-    }
+	public JwtAuthenticationFilter(JwtUtil jwtUtil, MemberRepository memberRepository,
+		FcmServiceImpl fcmService) {
+		this.jwtUtil = jwtUtil;
+		this.memberRepository = memberRepository;
+		this.fcmService = fcmService;
+		setFilterProcessesUrl("/auth/signin");
+	}
 
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-        HttpServletResponse response) throws
-        AuthenticationException {
-        log.info("로그인 시도");
-        try {
-            SigninRequest requestDto = new ObjectMapper().readValue(request.getInputStream(),
-                SigninRequest.class);
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request,
+		HttpServletResponse response) throws
+		AuthenticationException {
+		log.info("로그인 시도");
+		try {
+			SigninRequest requestDto = new ObjectMapper().readValue(request.getInputStream(),
+				SigninRequest.class);
 
-            Member member = memberRepository.findByEmail(requestDto.getEmail())
-                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저 이메일입니다."));
+			Member member = memberRepository.findByEmail(requestDto.getEmail())
+				.orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저 이메일입니다."));
 
-            if (member.getDeletedAt() != null) {
-                throw new GlobalException(DELETED_USER);
-            }
+			if (member.getDeletedAt() != null) {
+				throw new GlobalException(DELETED_USER);
+			}
 
-            UsernamePasswordAuthenticationToken authToken =
-                new UsernamePasswordAuthenticationToken(
-                    requestDto.getEmail(),
-                    requestDto.getPassword(),
-                    null
-                );
+			UsernamePasswordAuthenticationToken authToken =
+				new UsernamePasswordAuthenticationToken(
+					requestDto.getEmail(),
+					requestDto.getPassword(),
+					null
+				);
 
-            authToken.setDetails(requestDto.getFcmToken());
+			authToken.setDetails(requestDto.getFcmToken());
 
-            return getAuthenticationManager().authenticate(authToken);
+			return getAuthenticationManager().authenticate(authToken);
 
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e.getMessage());
-        }
-    }
+		} catch (IOException e) {
+			log.error(e.getMessage());
+			throw new RuntimeException(e.getMessage());
+		}
+	}
 
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-        HttpServletResponse response, FilterChain chain, Authentication authResult)
-        throws IOException,
-        ServletException {
-        log.info("로그인 성공 및 JWT 생성");
-        MemberRole role = ((UserDetailsImpl) authResult.getPrincipal()).getMember().getMemberRole();
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request,
+		HttpServletResponse response, FilterChain chain, Authentication authResult)
+		throws IOException,
+		ServletException {
+		log.info("로그인 성공 및 JWT 생성");
+		MemberRole role = ((UserDetailsImpl) authResult.getPrincipal()).getMember().getMemberRole();
 
-        String token = jwtUtil.createToken(
-            ((UserDetailsImpl) authResult.getPrincipal()).getMember().getId(),
-            ((UserDetailsImpl) authResult.getPrincipal()).getMember().getEmail(),
-            role);
-        jwtUtil.addJwtToCookie(token, response);
+		String token = jwtUtil.createToken(
+			((UserDetailsImpl) authResult.getPrincipal()).getMember().getId(),
+			((UserDetailsImpl) authResult.getPrincipal()).getMember().getEmail(),
+			((UserDetailsImpl) authResult.getPrincipal()).getMember().getImage(),
+			role);
+		jwtUtil.addJwtToCookie(token, response);
 
 		// FCM 토큰 처리
 		String fcmToken = (String) authResult.getDetails();
@@ -98,13 +100,13 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 				fcmToken
 			);
 		}
-    }
+	}
 
-    @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request,
-        HttpServletResponse response, AuthenticationException failed)
-        throws IOException, ServletException {
-        log.info("로그인 실패");
-        response.setStatus(401);
-    }
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request,
+		HttpServletResponse response, AuthenticationException failed)
+		throws IOException, ServletException {
+		log.info("로그인 실패");
+		response.setStatus(401);
+	}
 }
